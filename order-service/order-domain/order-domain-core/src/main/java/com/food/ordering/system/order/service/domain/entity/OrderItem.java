af16bef38ca0027894bd15b056fb3fb70c11cdc7
Christophe Boschmans <cboschmans@protonmail.com>
2024-02-21 11:05:04 +0100

    order  entities valueobjects

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -0,0 +1,82 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.BaseEntity;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.order.service.domain.valueobject.OrderItemId;
+
+public class OrderItem extends BaseEntity<OrderItemId> {
+    private OrderId orderId;
+    private final Product product;
+    private final int quantity;
+    private final Money price;
+    private final Money subTotal;
+
+    private OrderItem(Builder builder) {
+        product = builder.product;
+        super.setId(builder.orderItemId);
+        quantity = builder.quantity;
+        price = builder.price;
+        subTotal = builder.subTotal;
+    }
+
+
+    public OrderId getOrderId() {
+        return orderId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public Money getSubTotal() {
+        return subTotal;
+    }
+
+    void initializeOrderItem(OrderId orderId, OrderItemId orderItemId) {
+        this.orderId = orderId;
+        super.setId(orderItemId);
+    }
+
+    boolean isPriceValid() {
+        return price.IsGreaterThanZero() &&
+                price.equals(product.getPrice()) &&
+                price.multiply(quantity).equals(subTotal);
+    }
+
+    public static final class Builder {
+        private final Product product;
+        private OrderItemId orderItemId;
+        private final int quantity;
+        private final Money price;
+        private final Money subTotal;
+
+        private Builder(Product product, int quantity, Money price, Money subTotal) {
+            this.product = product;
+            this.quantity = quantity;
+            this.price = price;
+            this.subTotal = subTotal;
+        }
+
+        public static Builder builder(Product product, int quantity, Money price, Money subTotal) {
+            return new Builder(product, quantity, price, subTotal);
+        }
+
+        public Builder orderItemId(OrderItemId val) {
+            orderItemId = val;
+            return this;
+        }
+
+        public OrderItem build() {
+            return new OrderItem(this);
+        }
+    }
+}
